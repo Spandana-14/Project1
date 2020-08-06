84a8d09314bfd7ce10267550dfb1c3d6d69b9da7
Spandana-14 <61977998+Spandana-14@users.noreply.github.com>
2020-08-06 17:43:34 +0530

    area

---
+++ Area(circle).py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import math
+r=float(input("Enter the radius of circle:"))
+area=math.pi*r*r
+print("The area of the circle with radius " +str(r)+ " is: " +str(area) )
+
+
+# In[ ]:
+
+
+
+
