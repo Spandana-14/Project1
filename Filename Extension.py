c0ee92a0f3cab67efcd633610ef8ca3818e5b4af
Spandana-14 <61977998+Spandana-14@users.noreply.github.com>
2020-08-07 21:50:23 +0530

    Extension

---
+++ Filename Extension.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+file=str(input("Enter the filename :"))
+ext=file.split('.')
+if(ext[1]== 'py'):
+    print("The extension of the file is: 'Python' ")
+if(ext[1]== 'java'):
+    print("The extension of the file is: 'Java' ")
+if(ext[1]== 'c'):
+    print("The extension of the file is: 'C' ")
+if(ext[1]== 'cpp'):
+    print("The extension of the file is: 'C++(C Plus Plus)' ")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
